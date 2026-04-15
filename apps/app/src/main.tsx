607661e1288272673287957209684b16de7a6739
Adrià <adriablancafort@gmail.com>
2026-04-15 23:23:36 +0200

    read react query

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { BrowserRouter } from "react-router"
 import { createRoot } from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import { AppRoutes } from "@/routes"
 import { DebtorsProvider } from "@/context/debtors-context"
@@ -10,17 +11,21 @@ import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { TooltipProvider } from "@workspace/ui/components/tooltip"
 import { Toaster } from "@workspace/ui/components/sonner"
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <DebtorsProvider>
-        <ThemeProvider defaultTheme="light" storageKey="vexor-theme">
-          <TooltipProvider>
-            <AppRoutes />
-            <Toaster position="top-center" richColors />
-          </TooltipProvider>
-        </ThemeProvider>
-      </DebtorsProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <DebtorsProvider>
+          <ThemeProvider defaultTheme="light" storageKey="vexor-theme">
+            <TooltipProvider>
+              <AppRoutes />
+              <Toaster position="top-center" richColors />
+            </TooltipProvider>
+          </ThemeProvider>
+        </DebtorsProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
