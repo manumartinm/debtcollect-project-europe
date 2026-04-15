d7b2385d46734864315401e511335e55a56cdfd3
Adrià <adriablancafort@gmail.com>
2026-04-15 20:57:28 +0200

    api cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,18 @@
+import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
+const frontendUrl = process.env.FRONTEND_URL
 
 const app = new Hono()
+
+app.use(
+  '*',
+  cors({
+    origin: frontendUrl ?? '*',
+  })
+)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
